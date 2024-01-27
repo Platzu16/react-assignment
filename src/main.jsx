b2b341a7a874ea889019de172db95a15c975a7ef
James Platino <james.platino.ipi@prulifeuk.com.ph>
2024-01-28 02:09:40 +0800

    change page name

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import User from "./pages/User";
+import User from "./pages/UserPage";
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
